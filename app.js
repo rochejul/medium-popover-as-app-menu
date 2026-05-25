034d02cb1aa07b81115060728934150f2c37a1a7
Julien Roche <roche.jul@gmail.com>
2026-05-25 14:26:39 +0200

    fix: step 6 - keyboard navigation

--- app.js
+++ app.js
@@ -7,17 +7,30 @@ async function setup() {
   subcontainers.forEach((subcontainer) => {
     const subpopover = subcontainer.querySelector('[popover]');
 
-    // Event to show the subpopover when the mouse moves over
-    subcontainer.addEventListener('pointerover', (event) => {
-      subcontainer.classList.add('menu__subcontainer--active');
-      event.stopPropagation();
-      subpopover.showPopover();
+    [
+      'pointerover', // Event to show the subpopover when the mouse moves over
+      'focusin', // Event to make the subpopover keyboard-accessible
+    ].forEach((eventName) => {
+      subcontainer.addEventListener(eventName, (event) => {
+        subcontainer.classList.add('menu__subcontainer--active');
+        event.stopPropagation();
+        subpopover.showPopover();
+      });
     });
 
     // Event to hide the subpopover when the mouse moves out
     subcontainer.addEventListener('pointerleave', (event) => {
       subcontainer.classList.remove('menu__subcontainer--active');
       subpopover.hidePopover();
+    });
+
+    // Event to hide the subpopover with keyboard-accessible
+    subcontainer.addEventListener('focusout', (event) => {
+      if (!subcontainer.contains(event.relatedTarget)) {
+        // event.relatedTarget contains the active element
+        subcontainer.classList.remove('menu__subcontainer--active');
+        subpopover.hidePopover();
+      }
     });
   });
 
