f7c670e9ef2ed2a2de7c0591dc3b792eeaeab5bf
Julien Roche <roche.jul@gmail.com>
2026-05-25 14:07:01 +0200

    feat: step 2 - add an entry with a second level

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 async function setup() {
-  // TODO
+  const mainpopover = document.querySelector('.menu__mainpopover');
+  const subcontainers = Array.from(
+    document.querySelectorAll('.menu__subcontainer'),
+  );
+
+  subcontainers.forEach((subcontainer) => {
+    const subpopover = subcontainer.querySelector('[popover]');
+
+    // Event to show the subpopover when the mouse moves over
+    subcontainer.addEventListener('pointerover', (event) => {
+      event.stopPropagation();
+      subpopover.showPopover();
+    });
+
+    // Event to hide the subpopover when the mouse moves out
+    subcontainer.addEventListener('pointerleave', (event) => {
+      subpopover.hidePopover();
+    });
+  });
+
+  // Events to hide the popover menus when an option is selected in either of them
+  mainpopover.addEventListener('pointerdown', () => {
+    subcontainers.forEach((subcontainer) => {
+      const subpopover = subcontainer.querySelector('[popover]');
+      subpopover.hidePopover();
+    });
+
+    mainpopover.hidePopover();
+  });
 }
 
 if (document.readyState === 'loading') {
